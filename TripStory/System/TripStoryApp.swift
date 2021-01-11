0dc9f6aaaa7fc9ceeca934fdfefd2d6484298779
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-11 16:37:51 +0900

    Update TripStoryApp.swift

--- TripStory/System/TripStoryApp.swift
+++ TripStory/System/TripStoryApp.swift
@@ -9,15 +9,39 @@ import SwiftUI
 import Combine
 import EnvironmentOverrides
 
+typealias NotificationPayload = [AnyHashable: Any]
+typealias FetchCompletion = (UIBackgroundFetchResult) -> Void
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    //func application
+}
+
 @main
 struct TripStoryApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
     
-    let environment = AppEnvironment.bootstrap()
+    let environment: AppEnvironment
+    var systemEventsHandler: SystemEventsHandler?
+    
+    init() {
+        environment = AppEnvironment.bootstrap()
+        systemEventsHandler = environment.systemEventsHandler
+    }
     
     var body: some Scene {
         WindowGroup {
             ContentView().inject(environment.container)
         }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active: systemEventsHandler?.sceneDidBecomeActive()
+            case .inactive: systemEventsHandler?.sceneWillResignActive()
+            case .background: return
+            @unknown default: return
+            }
+        }
     }
+    
     
 }
