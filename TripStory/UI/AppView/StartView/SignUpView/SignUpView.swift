3ccd81f69cb63a05ca6fe299164c573e9ec59323
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-11 23:03:06 +0900

    Create SignUpView.swift

---
+++ TripStory/UI/AppView/StartView/SignUpView/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  TripStory
+//
+//  Created by Gon on 2021/01/11.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
