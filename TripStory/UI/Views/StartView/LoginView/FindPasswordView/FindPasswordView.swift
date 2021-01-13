27f9b97de0cdbde452654c2ee020ee3bcecdd82a
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-13 10:49:49 +0900

    :lipstick: Add FindPasswordView.swift

---
+++ TripStory/UI/Views/StartView/LoginView/FindPasswordView/FindPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  FindPasswordView.swift
+//  TripStory
+//
+//  Created by Gon on 2021/01/13.
+//
+
+import SwiftUI
+
+struct FindPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindPasswordView()
+    }
+}
