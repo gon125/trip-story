6c2fc7b11f2662366d6ffa821b485817b8948e75
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-13 13:17:26 +0900

    Update FindPasswordView.swift

--- TripStory/UI/Views/StartView/LoginView/FindPasswordView/FindPasswordView.swift
+++ TripStory/UI/Views/StartView/LoginView/FindPasswordView/FindPasswordView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct FindPasswordView: View {
+    @Binding var id: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.major.ignoresSafeArea()
+            VStack(alignment: .leading) {
+                Spacer()
+                TextField("UserID", text: $id)
+                    .modifier(TextFiledModifier())
+                Spacer().frame(height: 160)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Find Password")
+                        .modifier(ButtonModifier())
+                })
+                Spacer()
+            }
+            .padding(.horizontal, .horizontalPadding)
+        }
+        .navigationTitle("Find Password")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+#if DEBUG
 struct FindPasswordView_Previews: PreviewProvider {
     static var previews: some View {
-        FindPasswordView()
+        FindPasswordView(id: .constant("test"))
+            .environment(\.locale, .init(identifier: "ko"))
     }
 }
+#endif
