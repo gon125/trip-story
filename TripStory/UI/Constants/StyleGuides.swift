c5be28135e6631bc433659ab353007cc27a90887
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-13 10:46:59 +0900

    Create StyleGuides.swift

---
+++ TripStory/UI/Constants/StyleGuides.swift
@@ -0,0 +1,19 @@
+//
+//  StyleGuides.swift
+//  TripStory
+//
+//  Created by Gon on 2021/01/12.
+//
+import SwiftUI
+
+// layout styles
+extension CGFloat {
+    public static let horizontalPadding: CGFloat = 25
+    
+}
+
+//button styles
+extension CGFloat {
+    public static let buttonCornerRadius: CGFloat = 10
+    public static let buttonMaxHeight: CGFloat = 47
+}
