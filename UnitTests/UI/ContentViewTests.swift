02b94b9df6e9d8495e900263a6d01f2161615ccf
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-14 17:30:45 +0900

    :white_check_mark: Update ContentViewTests

--- UnitTests/UI/ContentViewTests.swift
+++ UnitTests/UI/ContentViewTests.swift
@@ -7,14 +7,21 @@
 
 import XCTest
 import ViewInspector
+
 @testable import TripStory
 
 extension ContentView: Inspectable { }
+extension StartView: Inspectable { }
 
 final class ContentViewTests: XCTestCase {
-    
+
     func test_content_for_tests() throws {
-        //let sut = ContentView(container: .defa)
+        let sut = ContentView(isRunningTests: true)
+        XCTAssertNoThrow(try sut.inspect().group().text(0))
     }
-    
+
+    func test_content_for_build() throws {
+        let sut = ContentView(isRunningTests: false)
+        XCTAssertNoThrow(try sut.inspect().group().zStack(0).anyView(0))
+    }
 }
