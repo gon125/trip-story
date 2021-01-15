6bd5bf737f1556a2d9a136f2169755aec8442192
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-15 17:27:30 +0900

    Rename LoadingButton -> LoadingButtonView

---
+++ TripStory/UI/Components/LoadingButtonView.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingButton.swift
+//  TripStory
+//
+//  Created by Gon on 2021/01/15.
+//
+
+import SwiftUI
+
+struct LoadingButtonView: View {
+    let title: String
+    @Binding var isLoading: Bool
+
+    init(_ title: String, isLoading: Binding<Bool> = .init(.constant(false))!) {
+        self.title = title
+        self._isLoading = isLoading
+    }
+
+    var body: some View {
+        self.content
+            .modifier(ButtonModifier())
+    }
+
+    @ViewBuilder private var content: some View {
+        if isLoading {
+            ProgressView()
+        } else {
+            Text(title)
+        }
+    }
+}
+
+#if DEBUG
+struct LoadingButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingButtonView("test")
+    }
+}
+#endif
