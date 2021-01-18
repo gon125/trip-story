69ef0e8dfc69e3524e663db431338f292089fc6b
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-18 16:24:58 +0900

    Rename TextFieldModifier -> TextFieldModifiers

--- TripStory/UI/ViewModifiers/TextFieldModifiers.swift
+++ TripStory/UI/ViewModifiers/TextFieldModifiers.swift
@@ -39,7 +39,31 @@ struct LoginTextFiledModifier: ViewModifier {
 
     }
 
-    private var stateUpdate: AnyPublisher<AppState.LoginState, Never> {
+    private var stateUpdate: AnyPublisher<LoginState, Never> {
         injected.appState.updates(for: \.loginState)
+    }
+}
+
+struct TextFiledModifier: ViewModifier {
+    @Binding private var isValid: Bool
+    @Binding private var description: String
+
+    init(isValid: Binding<Bool> = .init(.constant(true))!, description: Binding<String> = .init(.constant(""))!) {
+        self._isValid = isValid
+        self._description = description
+    }
+
+    func body(content: Content) -> some View {
+        VStack(alignment: .leading) {
+            content.foregroundColor(isValid ? Color.white : Color.red)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .font(.textInputTitle)
+            Divider()
+                .frame(height: 2)
+                .background(isValid ? Color.white : Color.red)
+            Text(LocalizedStringKey(description)).font(.textInputDescription).foregroundColor(.red)
+
+        }
     }
 }
