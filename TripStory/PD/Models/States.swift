5f3fceada57a8431d1e89ce6f98af049d3adbf0d
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-18 16:24:27 +0900

    Create States.swift

---
+++ TripStory/PD/Models/States.swift
@@ -0,0 +1,50 @@
+//
+//  States.swift
+//  TripStory
+//
+//  Created by Gon on 2021/01/17.
+//
+
+protocol CustomState: Equatable { }
+
+enum LoginState: CustomState {
+
+    case notRequested
+    case isInProgress
+    case success
+    case failed(LoginError)
+}
+
+enum SignupState: CustomState {
+
+    case notRequested
+    case isInProgress
+    case success
+    case failed(SignupError)
+}
+
+enum PasswordState: CustomState, CustomStringConvertible {
+    case valid
+    case empty
+    case noMatch
+
+    var description: String {
+        switch self {
+        case .valid: return ""
+        case .empty: return "Enter a password."
+        case .noMatch: return "Those passwords didin't match. Try again."
+        }
+    }
+}
+
+enum UsernameState: CustomState, CustomStringConvertible {
+    case valid
+    case empty
+
+    var description: String {
+        switch self {
+        case .valid: return ""
+        case .empty: return "Enter a username."
+        }
+    }
+}
