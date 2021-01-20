11dea97019e6d3495d0efe2d46c417f2a4192256
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-20 19:35:17 +0900

    Update TextFieldModifiers.swift

--- TripStory/UI/ViewModifiers/TextFieldModifiers.swift
+++ TripStory/UI/ViewModifiers/TextFieldModifiers.swift
@@ -8,42 +8,6 @@
 import SwiftUI
 import Combine
 
-struct LoginTextFiledModifier: ViewModifier {
-    @Environment(\.injected) private var injected: DIContainer
-    @State private var errorDetected: Bool = false
-    let checkingError: LoginError
-    @State private var errorDescription: LocalizedStringKey = ""
-
-    func body(content: Content) -> some View {
-        VStack(alignment: .leading) {
-            content.foregroundColor(errorDetected ? Color.red : Color.white)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-                .font(.textInputTitle)
-            Divider()
-                .frame(height: 2)
-                .background(errorDetected ? Color.red : Color.white)
-            Text(errorDetected ? errorDescription : "").font(.textInputDescription).foregroundColor(.red)
-        }
-        .onReceive(stateUpdate) {
-            switch $0 {
-            case let .failed(error):
-                if checkingError == error {
-                    self.errorDetected = true
-                    errorDescription = LocalizedStringKey(error.description)
-                }
-            default:
-                self.errorDetected = false
-            }
-        }
-
-    }
-
-    private var stateUpdate: AnyPublisher<LoginState, Never> {
-        injected.appState.updates(for: \.loginState)
-    }
-}
-
 struct TextFieldModifier: ViewModifier {
     @Binding private var isValid: Bool
     @Binding private var description: String
