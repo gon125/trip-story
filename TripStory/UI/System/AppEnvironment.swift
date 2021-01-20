a2d1c34d0b915f9d86817b8a7d7bfb74b384fa80
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-01-20 19:34:54 +0900

    Update AppEnvironment.swift

--- TripStory/UI/System/AppEnvironment.swift
+++ TripStory/UI/System/AppEnvironment.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import Foundation
 
 struct AppEnvironment {
     let container: DIContainer
@@ -15,20 +16,45 @@ struct AppEnvironment {
 extension AppEnvironment {
     static func bootstrap() -> AppEnvironment {
         let appState = Store(AppState())
-        let interactors = configuredInteractors(appState: appState)
+        let session = configuredURLSession()
+        let repositories = configuredRepositories(session: session)
+        let services = configuredServices(repositories: repositories)
+        let interactors = configuredInteractors(appState: appState, services: services)
 
         let diContainer = DIContainer(appState: appState, interactors: interactors)
         let systemEventHandler = DefaultSystemEventHandler(container: diContainer)
         return AppEnvironment(container: diContainer, systemEventsHandler: systemEventHandler)
     }
 
-    private static func configuredInteractors(appState: Store<AppState>) -> DIContainer.Interactors {
+    private static func configuredInteractors(
+        appState: Store<AppState>,
+        services: DIContainer.Services
+    ) -> DIContainer.Interactors {
         let signupInteractor = DefaultSignupInteractor()
-        let loginInteractor = DefaultLoginInteractor(appState: appState)
+        let loginInteractor = DefaultLoginInteractor(
+            appState: appState,
+            authService: services.authenticationService
+        )
 
         return .init(
             signupInteractor: signupInteractor,
             loginInteractor: loginInteractor
         )
+    }
+
+    private static func configuredServices(repositories: DIContainer.Repositories) -> DIContainer.Services {
+        let authenticationService = DefaultAuthenticationService(repository: repositories.authenticationRepository)
+        return .init(authenticationService: authenticationService)
+    }
+
+    private static func configuredRepositories(session: URLSession) -> DIContainer.Repositories {
+
+        let baseURL = "http://localhost:3000"
+        let authenticationRepository = DefaultAuthenticationRepository(session: session, baseURL: baseURL)
+        return .init(authenticationRepository: authenticationRepository)
+    }
+
+    private static func configuredURLSession() -> URLSession {
+        return URLSession.shared
     }
 }
